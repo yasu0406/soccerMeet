77908dd5aa23be14ab27a33fd4c5795348ea0859
yasuhiro <yasucom4646@gmail.com>
2019-10-23 10:16:49 -0700

    Update babel.config.js to use spread

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    "env": {
+      "development": {
+        "plugins": ["react-hot-loader/babel"]
+      },
+      "test": {
+        "plugins": [
+          ["babel-plugin-webpack-alias-7", { "config": "./build_config/webpack.common.js" }],
+          "istanbul",
+          "dynamic-import-node",
+          "@babel/plugin-proposal-object-rest-spread"
+        ]
+      },
+      "production": {
+        "plugins": ["transform-react-remove-prop-types", ["recharts"]]
+      }
+    }
   };
 };
