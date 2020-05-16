673901dd7c1439a6c467571bf59a644f7acb16f5
yasuhiro <yasucom4646@gmail.com>
2020-05-15 22:27:36 -0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    "env": {
-      "test": {
-        "plugins": [
-          ["babel-plugin-webpack-alias-7", { "config": "./build_config/webpack.common.js" }],
-          "istanbul",
-          "dynamic-import-node",
-          "@babel/plugin-proposal-object-rest-spread"
-        ]
-      },
-      "production": {
-        "plugins": ["transform-react-remove-prop-types", ["recharts"]]
-      }
-    }
   };
 };
